fed7c247978114a1855f4532bc3fcccab507ec48
lorossi <contact@lorenzoros.si>
2021-03-21 16:49:38 +0100

    brightness calibration done

--- calibration/brightness-calibration/src/main.cpp
+++ calibration/brightness-calibration/src/main.cpp
@@ -1,9 +1,58 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define LED_NUMBER 22
+#define LED_PIN 5
+
+CRGB leds[LED_NUMBER];
+long int colors[4] = {0xFF0000, 0xB95000, 0xFFBB00, 0xFFFFFF};
+byte count = 0;
+
+void setup();
+void loop();
+
+void setup()
+{
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, LED_NUMBER)
+      .setCorrection(Tungsten40W);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void fillLeds(long int color)
+{
+  for (int i = 0; i < LED_NUMBER; i++)
+  {
+    byte brightness;
+    brightness = 255 / LED_NUMBER * i;
+
+    // color correction begins here
+    // basically, i did math
+    // some kind of lerp i don't know
+    // adds green and red to darker brightness
+    float percent;
+    percent = (float)brightness / 255.0;
+
+    byte green;
+    green = (color & 0x00FF00) >> 8;
+    long int dGreen;
+    dGreen = byte(green * percent) << 8;
+
+    byte red;
+    red = (color & 0xFF0000) >> 16;
+    long int dRed;
+    dRed = byte(red * percent) << 16;
+    color = color + byte(dGreen) + byte(dRed);
+    // color correction ends here
+
+    leds[i] = color;
+    leds[i].fadeToBlackBy(brightness);
+  }
+
+  FastLED.show();
+}
+
+void loop()
+{
+  fillLeds(colors[count % 4]);
+  count++;
+  delay(1000);
 }
