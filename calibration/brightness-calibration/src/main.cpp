948aa158c45557d0a0d149b1d4c6f64176c49bf3
lorossi <contact@lorenzoros.si>
2021-03-23 11:23:20 +0100

    fixed algorithm

--- calibration/brightness-calibration/src/main.cpp
+++ calibration/brightness-calibration/src/main.cpp
@@ -3,31 +3,57 @@
 
 #define LED_NUMBER 22
 #define LED_PIN 5
-#define MAX_BLEND 40
+#define MAX_BLEND 100     // max color blending (0-255)
+#define MIN_BRIGHTNESS 80 // cutoff brightness (0-255)
 
+CRGB offset = 0xFFFF00; // color that gets added to original one
 CRGB leds[LED_NUMBER];
-CRGB offset;
+
 long int colors[4] = {0xFF0000, 0xB93C00, 0xFFBB00, 0xFFFFFF};
-byte brightness = 40;
+byte brightness = 255;
+long int count = 0;
 
 void setup();
 void loop();
+float easing(float x);
+
+float easing(float x)
+{
+  // quadratic easing
+  return 1 - (1 - x) * (1 - x);
+}
 
 void setup()
 {
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, LED_NUMBER)
-      .setCorrection(Tungsten40W);
-  offset = 0xFFFF00;
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, LED_NUMBER);
 }
 
 void fillLeds()
 {
   for (int i = 0; i < LED_NUMBER; i++)
   {
-    float percent;
-    percent = map(brightness, 0, 255, MAX_BLEND, 0);
-    CRGB blended = blend(colors[i % 4], offset, percent);
+    // color correction starts here
+    CRGB blended;
+    float eased;
+
+    if (brightness <= MIN_BRIGHTNESS && count % 2 == 0)
+    {
+      // calculate percent
+      float percent = (float)brightness / MIN_BRIGHTNESS;
+      // ease percent
+      eased = (1 - easing(percent)) * MAX_BLEND;
+    }
+    else
+    {
+      // skip color correction if brightness if over the cutoff vlue
+      eased = 1;
+    }
+
+    // blend colors
+    blended = blend(colors[i % 3], offset, eased);
+    // add color
     leds[i] = blended;
+    // color correction ends here
   }
 
   FastLED.setBrightness(brightness);
@@ -36,6 +62,8 @@ void fillLeds()
 
 void loop()
 {
+  brightness = 20;
+  count++;
   fillLeds();
   delay(1000);
 }
