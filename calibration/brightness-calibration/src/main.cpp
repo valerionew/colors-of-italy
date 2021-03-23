4ec3dd8c579af985778df77fa3dd41e78436c1aa
lorossi <contact@lorenzoros.si>
2021-03-23 12:45:59 +0100

    added comments

--- calibration/brightness-calibration/src/main.cpp
+++ calibration/brightness-calibration/src/main.cpp
@@ -17,6 +17,8 @@ void setup();
 void loop();
 float easing(float x);
 
+// x: 0->1
+// return: 0->1
 float easing(float x)
 {
   // quadratic easing
@@ -41,11 +43,15 @@ void fillLeds()
       // calculate percent
       float percent = (float)brightness / MIN_BRIGHTNESS;
       // ease percent
+      // we need to invert it (1-easing) in order to get 1 for low brightness values
+      // and 0 for high brightness values, so that more color gets blended at lower
+      // brightness to compensate the unbalancing of the leds
       eased = (1 - easing(percent)) * MAX_BLEND;
     }
     else
     {
-      // skip color correction if brightness if over the cutoff vlue
+      // skip color correction if brightness if over the cutoff value (or, in this case,
+      // count is even)
       eased = 1;
     }
 
@@ -55,8 +61,9 @@ void fillLeds()
     leds[i] = blended;
     // color correction ends here
   }
-
+  // set global brightness
   FastLED.setBrightness(brightness);
+  // actually show leds
   FastLED.show();
 }
 
