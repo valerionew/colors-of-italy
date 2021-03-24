7728fd58837d8c2225c28832b79c28ead1369aa9
valerionew <valerionappi995@gmail.com>
2021-03-24 15:01:33 +0100

    Brand new ota code brand new lorenzo

--- ota/ota-test/src/main.cpp
+++ ota/ota-test/src/main.cpp
@@ -1,115 +1,91 @@
-
-#include <Arduino.h>     // built in library
-#include <WiFi.h>        // built in library
-#include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson
-#include <HTTPClient.h>  // built in library
-#include "certs.h"
+// This sketch provide the functionality of OTA Firmware Upgrade
+#include "WiFi.h"
 #include "HttpsOTAUpdate.h"
+// This sketch shows how to implement HTTPS firmware update Over The Air.
+// Please provide your WiFi credentials, https URL to the firmware image and the server certificate.
 
-#define SSID "GatesNuovoOrdineMondiale"
-#define PASSWORD "piramidediastana"
-#define VERSION "0.0.9"
+static const char *ssid     = "GatesNuovoOrdineMondiale";  // your network SSID (name of wifi network)
+static const char *password = "piramidediastana"; // your network password
 
-#define OTA_VERSION_REQUEST_URL "https://vaccinocovid19.live/get/ota_update"
+static const char *url = "https://vaccinocovid19.live:443/ota/1.0.0.bin"; //state url of your firmware image
 
-extern const char server_cert_pem_start[] asm("_binary_server_certs_certs_pem_start");
-extern const char server_cert_pem_end[] asm("_binary_server_certs_certs_pem_end");
+static const char *server_certificate = "-----BEGIN CERTIFICATE-----\n"\
+                "MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/\n"\
+                "MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n"\
+                "DkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow\n"\
+                "PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD\n"\
+                "Ew5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\n"\
+                "AN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O\n"\
+                "rz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq\n"\
+                "OLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b\n"\
+                "xiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw\n"\
+                "7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD\n"\
+                "aeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV\n"\
+                "HQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG\n"\
+                "SIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69\n"\
+                "ikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr\n"\
+                "AvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz\n"\
+                "R8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5\n"\
+                "JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo\n"\
+                "Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ\n"\
+                "-----END CERTIFICATE-----\n";
 
-WiFiClient client;
+static HttpsOTAStatus_t otastatus;
 
 void HttpEvent(HttpEvent_t *event)
 {
-  switch (event->event_id)
-  {
-  case HTTP_EVENT_ERROR:
-    Serial.println("Http Event Error");
-    break;
-  case HTTP_EVENT_ON_CONNECTED:
-    Serial.println("Http Event On Connected");
-    break;
-  case HTTP_EVENT_HEADER_SENT:
-    Serial.println("Http Event Header Sent");
-    break;
-  case HTTP_EVENT_ON_HEADER:
-    Serial.printf("Http Event On Header, key=%s, value=%s\n", event->header_key, event->header_value);
-    break;
-  case HTTP_EVENT_ON_DATA:
-    break;
-  case HTTP_EVENT_ON_FINISH:
-    Serial.println("Http Event On Finish");
-    break;
-  case HTTP_EVENT_DISCONNECTED:
-    Serial.println("Http Event Disconnected");
-    break;
-  }
+    switch(event->event_id) {
+        case HTTP_EVENT_ERROR:
+            Serial.println("Http Event Error");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            Serial.println("Http Event On Connected");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            Serial.println("Http Event Header Sent");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            Serial.printf("Http Event On Header, key=%s, value=%s\n", event->header_key, event->header_value);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            Serial.println("Http Event On Finish");
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            Serial.println("Http Event Disconnected");
+            break;
+    }
 }
 
-void setup()
-{
-  Serial.begin(115200);
+void setup(){
 
-  WiFi.begin(SSID, PASSWORD);
-  Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(100);
-  };
-  Serial.println("WiFi connected");
+    Serial.begin(115200);
+    Serial.print("Attempting to connect to SSID: ");
+    WiFi.begin(ssid, password);
 
-  HTTPClient http;
-  int httpResponseCode;
-  http.begin(OTA_VERSION_REQUEST_URL);
-  httpResponseCode = http.GET();
-
-  Serial.println();
-  if (httpResponseCode > 0)
-  {
-    StaticJsonDocument<256> doc;
-    // buffer size calculated here: https://arduinojson.org/v6/assistant/
-    String json_data = http.getString();
-    // parse JSON data
-    DeserializationError err = deserializeJson(doc, json_data);
-
-    if (!err)
-    {
-      JsonObject obj = doc.as<JsonObject>();
-      String ota_version = obj["version"];
-      Serial.print("ota version: ");
-      Serial.print(ota_version);
-      Serial.println();
-      // get remote version andcheck if the current version is different
-      if (ota_version != VERSION)
-      {
-        // get .bin url and update for real
-        String ota_url = obj["url"];
-
-        Serial.print("ota url: ");
-        Serial.println(ota_url);
-
-        HttpsOTA.onHttpEvent(HttpEvent);
-        Serial.println("Starting OTA");
-        HttpsOTA.begin(ota_url, CERT_PEM);
-
-        Serial.println("Config set...");
-
-        otastatus = HttpsOTA.status();
-        if (otastatus == HTTPS_OTA_SUCCESS)
-        {
-          Serial.println("Firmware written successfully. To reboot device, call API ESP.restart() or PUSH restart button on device");
-        }
-        else if (otastatus == HTTPS_OTA_FAIL)
-        {
-          Serial.println("Firmware Upgrade Fail");
-        }
-            }
+    // attempt to connect to Wifi network:
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(1000);
     }
 
-    doc.clear();
-  }
-  http.end();
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    
+    HttpsOTA.onHttpEvent(HttpEvent);
+    Serial.println("Starting OTA");
+    HttpsOTA.begin(url, server_certificate); 
+
+    Serial.println("Please Wait it takes some time ...");
 }
 
-void loop()
-{
+void loop(){
+    otastatus = HttpsOTA.status();
+    if(otastatus == HTTPS_OTA_SUCCESS) { 
+        Serial.println("Firmware written successfully. To reboot device, call API ESP.restart() or PUSH restart button on device");
+    } else if(otastatus == HTTPS_OTA_FAIL) { 
+        Serial.println("Firmware Upgrade Fail");
+    }
+    delay(1000);
 }
