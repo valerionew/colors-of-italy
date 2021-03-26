1d504c3bf024f5caa4867c7c8e45f0f504508313
lorossi <contact@lorenzoros.si>
2021-03-26 12:55:44 +0100

    cleaned code, added credits

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(() => {
   let input = $(".form #emailinput");
   let send = $(".form #send");
@@ -8,18 +6,52 @@ $(document).ready(() => {
   let regex = /[^@]+@[^@]+\.[^@]+/g;
   let url = "https://vaccinocovid19.live/post/newsletter";
 
+  const removeDefaultText = () => {
+    text = input.val();
+    if (text.includes(default_text)) {
+      // remove default text
+      input.val(text.replace(default_text, ""));
+      input.css({ "opacity": 1 });
+    } else if (text === "") {
+      // reset to default
+      input.val(default_text);
+      input.css({ "opacity": 0.5 });
+    }
+  };
+
+  const showError = (error) => {
+    confirmation.css({ "background-color": "Brown" });
+    confirmation.html("<p>Errore interno del server.<br>Si prega di provare più tardi.</p>");
+    showConfirmation();
+  };
+
+  const showSuccess = (success) => {
+    if (success.response != 200) {
+      showError();
+      return;
+    }
+
+    confirmation.css({ "background-color": "lightgreen" });
+    confirmation.html("<p>La tua email è stata salvata.<br>Verrai presto ricontattato!</p>");
+    showConfirmation();
+  };
+
+  const showConfirmation = () => {
+    confirmation.css({ "display": "flex" });
+    confirmation.animate({
+      "height": "4rem"
+    }, 500);
+  };
+
   // preload text
   let default_text = "Inserisci la tua email";
   input.val(default_text);
   input.css({ "opacity": 0.5 });
 
   // remove preloaded text
-  input.click(() => {
-    if (input.val() === default_text) {
-      input.val("");
-      input.css({ "opacity": 1 });
-    }
-  });
+  input.on("input", removeDefaultText);
+  // i cannot make this work with "on" so i bind it to a different event
+  input.click(removeDefaultText);
 
   input.on("input", () => {
     send.attr("disabled", !input.val().match(regex));
@@ -52,32 +84,4 @@ $(document).ready(() => {
     });
 
   });
-
-
-
-  const showError = (error) => {
-    //console.log("error", error);
-    confirmation.css({ "background-color": "Brown" });
-    confirmation.html("<p>Errore interno del server.<br>Si prega di provare più tardi.</p>");
-    showConfirmation();
-  };
-
-  const showSuccess = (success) => {
-    //console.log("success", success);
-    if (success.response != 200) {
-      showError();
-      return;
-    }
-
-    confirmation.css({ "background-color": "lightgreen" });
-    confirmation.html("<p>La tua email è stata salvata.<br>Verrai presto ricontattato!</p>");
-    showConfirmation();
-  };
-
-  const showConfirmation = () => {
-    confirmation.css({ "display": "flex" });
-    confirmation.animate({
-      "height": "4rem"
-    }, 500);
-  };
 });
